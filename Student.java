0a62655d90ff81231d115ccce6afe608d3690a1f
Diksha <dikshasharma11oct@gmail.com>
2023-11-04 19:12:09 +0530

    jenkins java program

--- Student.java
+++ Student.java
@@ -1,8 +1,12 @@
 public class Student {
 
-    public void Student(){
+    public Student(){
         System.out.println("student class");
 
+    }
+
+    public void hello(){
+         System.out.println("Hi, I am student");
     }
     
 }
