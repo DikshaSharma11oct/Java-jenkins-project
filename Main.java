43881c82f3d5f3f936395a0bfaa6d682560bffcf
Diksha <dikshasharma11oct@gmail.com>
2023-11-04 19:05:05 +0530

    jenkins java program

--- Main.java
+++ Main.java
@@ -8,5 +8,8 @@ public class Main{
         System.out.println(new Date());
         System.out.println("this is to test second command on jenkins");
 
+        Student std=new Student();
+        std.hello();
+
     }
 }
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+
+    public void Student(){
+        System.out.println("student class");
+
+    }
+    
+}
